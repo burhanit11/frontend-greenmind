7e2e88a0d8368aeea78fe9663ce492f2e21b270d
Burhan Rabbani <burhanit11@gmail.com>
2024-09-28 11:50:38 +0500

    update

--- src/pages/products/Products.jsx
+++ src/pages/products/Products.jsx
@@ -1,7 +1,7 @@
 import { useDispatch, useSelector } from "react-redux";
-// import frame7 from "../../assets/Frame 7.png";
-// import frame8 from "../../assets/Frame 8.png";
-// import frame9 from "../../assets/Frame 9.png";
+import frame7 from "../../assets/Frame 7.png";
+import frame8 from "../../assets/Frame 8.png";
+import frame9 from "../../assets/Frame 9.png";
 import Footer from "../../components/footer/Footer";
 import Header from "../../components/header/Header";
 import { addToCart, removeFromCart } from "../../redux/userSlices/cartSlice";
@@ -9,21 +9,21 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import { API_BASE_URL } from "../../utils/constran";
 
-// const data = [
-//   {
-//     id: 1,
-//     title: "Natural Plants",
-//     img: frame7,
-//     price: "130$",
-//   },
-//   { id: 2, title: "Natural Plants", img: frame8, price: 20 },
-//   { id: 3, title: "Natural Plants", img: frame9, price: 50 },
-//   { id: 4, title: "Natural Plants", img: frame7, price: 60 },
-//   { id: 5, title: "Natural Plants", img: frame8, price: 100 },
-//   { id: 6, title: "Natural Plants", img: frame9, price: 10 },
-//   { id: 7, title: "Natural Plants", img: frame7, price: 5 },
-//   { id: 8, title: "Natural Plants", img: frame8, price: 15 },
-// ];
+const data = [
+  {
+    id: 1,
+    title: "Natural Plants",
+    img: frame7,
+    price: "130$",
+  },
+  { id: 2, title: "Natural Plants", img: frame8, price: 20 },
+  { id: 3, title: "Natural Plants", img: frame9, price: 50 },
+  { id: 4, title: "Natural Plants", img: frame7, price: 60 },
+  { id: 5, title: "Natural Plants", img: frame8, price: 100 },
+  { id: 6, title: "Natural Plants", img: frame9, price: 10 },
+  { id: 7, title: "Natural Plants", img: frame7, price: 5 },
+  { id: 8, title: "Natural Plants", img: frame8, price: 15 },
+];
 const AllProducts = () => {
   // const products = useSelector((state) => state.products);
   const [isLoading, setIsloading] = useState(false);
@@ -48,16 +48,12 @@ const AllProducts = () => {
     setIsloading(false);
   }, []);
 
-  console.log(product.products, "??");
   const handleAddToCart = (item) => {
     dispatch(addToCart(item));
-    console.log(item, "???????????????");
   };
   const handleRemoveToCart = (id) => {
     dispatch(removeFromCart(id));
   };
-
-  console.log(product, "product");
 
   if (isLoading) {
     return <h1 className="text-red-500">Loading...</h1>;
@@ -66,9 +62,9 @@ const AllProducts = () => {
     <>
       <Header />
       <div className="max-w-6xl mx-auto py-10 grid md:grid-cols-4  grid-cols-1 gap-8 pt-20">
-        {product?.products?.map((item) => (
+        {data.map((item) => (
           <div
-            key={item._id}
+            key={item.id}
             className="flex flex-col md:justify-start justify-center "
           >
             <img src={item.img} alt="" />
@@ -86,7 +82,7 @@ const AllProducts = () => {
                   {/* <FaArrowRightLong className="mx-2" /> */}
                 </button>
                 <button
-                  onClick={() => handleRemoveToCart(item._id)}
+                  onClick={() => handleRemoveToCart(item.id)}
                   className="bg-primary hover:bg-primary/80 flex justify-center items-center text-black p-2 rounded-md text-md"
                 >
                   remove to card
